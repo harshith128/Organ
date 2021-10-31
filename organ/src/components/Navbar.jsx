afa436bc53a8f9e82c60e3a91f0e4a178e500896
Sameel555 <sameel555@gmail.com>
2021-10-31 22:42:18 +0530

    React App created

---
+++ organ/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from "./navbar.module.css"
+
+export default function Navbar() {
+    
+    return (
+        <div className={styles.navbar}>
+            
+        </div>
+    )
+}
+
+
