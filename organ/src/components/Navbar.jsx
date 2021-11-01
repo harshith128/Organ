3bf19b306b22d254ebb5ba0b9c4fb0ad468656ee
sameel555 <82999575+sameel555@users.noreply.github.com>
2021-11-01 13:24:58 +0530

    Update Navbar.jsx

--- organ/src/components/Navbar.jsx
+++ organ/src/components/Navbar.jsx
@@ -1,14 +1,145 @@
-import React from 'react'
+.navbar {
+   position: fixed;
+   width: 100%;
+   height: 56px;
+   left: 100px;
+   top: 54px;
+   background: #FFFFFF;
+}
+.logo{
+    position: absolute;
+    left: 0%;
+    right: 12%;
+    top: 32.56%;
+    bottom: 0%;
+    color: #C50005;
 
-import styles from "./navbar.module.css"
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 113%;
+}
+.navflex{
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
 
-export default function Navbar() {
+    position: absolute;
+    left: 41.21%;
+    right: 15.37%;
+    top: 30.42%;
+    bottom: 32.08%;
+}
+.navflex div{
+    position: static;
+    width: 137px;
+    height: 21px;
+    left: 0px;
+    top: 0px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 21px;
     
-    return (
-        <div className={styles.navbar}>
-            
-        </div>
-    )
+    /* identical to box height */
+
+     text-align: center;
+
+    color: #17295E;
+}
+.vector{
+     position: static;
+     /* width: 137px;
+     height: 21px; */
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+
+     font-family: Poppins;
+     font-style: normal;
+     font-weight: normal;
+     font-size: 14px;
+    /* line-height: 21px; */
+
+     /* identical to box height */
+
+     text-align: center;
+
+     color: #17295E;
+}
+.reg{
+    color: #17295E;
+}
+:hover > .reg{
+    color: #ED2025;
+
+
+}
+.detailreg{
+    display: none;
+     color: #17295E;
+}
+.reg:hover > .detailreg{
+    display: block;
+}
+.detailreg1{
+    color: #17295E;
 }
 
+:hover>.detailreg1 {
+    color: #ED2025;
 
+}
+
+.detailreg2 {
+    color: #17295E;
+}
+:hover>.detailreg2 {
+    color: #ED2025;
+
+
+}
+.btnpledge{
+    position: absolute;
+    left: 70%;
+    right: 4.44%;
+    top: 20%;
+    bottom: 20%;
+    color: #FFFFFF;
+ 
+    background: #ED2025;
+    border-radius: 8px;
+}
+.lang{
+    position: absolute;
+    left: 55%;
+    right: 0.81%;
+    top: 31.67%;
+    bottom: 30.83%;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 21px;
+    /* identical to box height */
+
+    text-align: center;
+
+    color: #17295E;
+
+}
+.line{
+    position: absolute;
+    left: 0%;
+    right: 0%;
+    top: 100%;
+    bottom: 0%;
+
+    border: 1px solid #36487D;
+}
