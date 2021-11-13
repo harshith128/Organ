61868b8bcdfbdfcc7064d5737c1b3a1c13d7adeb
harshith128 <harshith_fw11_128@masai.school>
2021-11-13 16:00:07 +0530

    view report is done

--- organ/src/components/Thanks_bd.jsx
+++ organ/src/components/Thanks_bd.jsx
@@ -3,23 +3,30 @@ import "./thanks_bd.css"
 import Navbar from './navbar/Navbar'
 
 import Footer from './footer/Footer'
+import { Second_Component } from "./Second_Component";
 
 
 function Thanks_bd(){
+    const {user, hospital, token} = JSON.parse(localStorage.getItem("login"));
+
+    const handleLoginClick = () => {
+        window.location.pathname = "/login_brain_death";
+    }
 
 
     return(
 
         <div >
             <Navbar/>
+            <Second_Component  user={ user } hospital= { hospital } />
             <div className="thanks_main10">
                 <p className="p110">Thanks for Register!</p>
                 <div className="tbd11">
-                <p className="tbd12">Your Registration Number is-</p><p className="tbd10">TNORDGRF45</p>
+                {/* <p className="tbd12">Your Registration Number is-</p><p className="tbd10">TNORDGRF45</p> */}
                 </div>
-                <p  className="tbd10">You Wiil Get a Notification in your Registered Mail & Mobile Number When the Doner is Registered</p>
+                <p  className="tbd10">You Will Get a Notification in your Registered Mail & Mobile Number When the Doner is Registered</p>
                
-                <button className="b110"> Back to Login</button>
+                <button className="b110" onClick={handleLoginClick}> Back to Login</button>
                 <br/>
                 <br/>
                 
