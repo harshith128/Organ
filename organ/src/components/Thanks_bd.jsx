5f5e255186fe38a26b1afa86a900b539644256b2
harshith128 <harshith_fw11_128@masai.school>
2021-11-13 18:17:22 +0530

    pd done

--- organ/src/components/Thanks_bd.jsx
+++ organ/src/components/Thanks_bd.jsx
@@ -8,6 +8,7 @@ import { Second_Component } from "./Second_Component";
 
 function Thanks_bd(){
     const {user, hospital, token} = JSON.parse(localStorage.getItem("login"));
+    const { patientID } = JSON.parse(localStorage.getItem("patientID"));
 
     const handleLoginClick = () => {
         window.location.pathname = "/login_brain_death";
@@ -22,7 +23,7 @@ function Thanks_bd(){
             <div className="thanks_main10">
                 <p className="p110">Thanks for Register!</p>
                 <div className="tbd11">
-                {/* <p className="tbd12">Your Registration Number is-</p><p className="tbd10">TNORDGRF45</p> */}
+                <p className="tbd12">Your Registration Number is-</p><p className="tbd10">{ patientID }</p>
                 </div>
                 <p  className="tbd10">You Will Get a Notification in your Registered Mail & Mobile Number When the Doner is Registered</p>
                
